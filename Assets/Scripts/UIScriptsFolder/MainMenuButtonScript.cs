00536a103fad77e46679290ce98b5fed2b059c36
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2023-05-26 22:58:11 +0700

    Main menu update

--- Assets/Scripts/UIScriptsFolder/MainMenuButtonScript.cs
+++ Assets/Scripts/UIScriptsFolder/MainMenuButtonScript.cs
@@ -5,24 +5,31 @@ using UnityEngine.UI;
 
 public class MainMenuButtonScript : MonoBehaviour
 {
-    public GameObject tutorialPanel, mainMenuPanel, playMenuPanel;
+    public GameObject tutorialPanel, mainMenuPanel, playMenuPanel, loginPanel, quickMatchPanel, matchTypePanel;
 
     // Start is called before the first frame update
     void Start()
     {
         tutorialPanel.SetActive(false);
         playMenuPanel.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        loginPanel.SetActive(false);
+        quickMatchPanel.SetActive(false);
+        matchTypePanel.SetActive(false);
     }
 
     public void OpenPlayMenu()
     {
         playMenuPanel.SetActive(true);
+    }
+
+    public void QuickMatch()
+    {
+        quickMatchPanel.SetActive(true);
+    }
+
+    public void LobbyMatch()
+    {
+        loginPanel.SetActive(true);
     }
 
     public void ClosePlayMenu()
@@ -40,9 +47,18 @@ public class MainMenuButtonScript : MonoBehaviour
         tutorialPanel.SetActive(false);
     }
 
-
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+    public void OpenThisGameObject(GameObject gameObject)
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void CloseThisGameObject(GameObject gameObject)
+    {
+        gameObject.SetActive(false);
     }
 }
